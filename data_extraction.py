235aabb22a3e899dedd41151e8faf7c5bb1d8439
Victor-Kola <victor.kolawole@hotmail.com>
2023-11-01 17:05:55 +0000

    Defined class method to read table data in a pandas dataframe

--- data_extraction.py
+++ data_extraction.py
@@ -1,1 +1,19 @@
+import pandas as pd
+from database_utils import DatabaseConnector
+
+list_of_tables = DatabaseConnector.list_db_tables()
+print(list_of_tables)
+
 class DataExtractor:
+    @staticmethod
+    def read_rds_table(table_name, db_connector = DatabaseConnector()):
+        engine = db_connector.init_db_engine()
+        if engine is not None:
+            df= pd.read_sql_table(table_name, engine)
+            return df
+        else:
+            print("The engine failed to initialise")
+            return None
+
+user_data = DataExtractor.read_rds_table('legacy_users')
+print(user_data)
